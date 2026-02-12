bbede2a7548578ff785af5a1671cbc024deee008
TranDuc511 <manhducpc05@gmail.com>
2026-02-12 15:00:57 +0700

    fix and update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dataset
-Dataset for project inno
+1. Read two examples in folder /dataset_sample to understand format
+2. Final dataset can be used for training located in /dataset, which include merged and unmerged version
+3. Data cleaning and processing code are in /datacorrection 
+
+Cautions:
+You must merge customers and transactions into one dataset before training
---
+++ datacorrection/merge.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import random
+
+customerpath = r"C:\Users\Admin\Documents\dataset\dataset\customers.json"
+transactionpath = r"C:\Users\Admin\Documents\dataset\dataset\transactions.json"
+
+cust = pd.read_json(customerpath)
+trans = pd.read_json(transactionpath)
+
+#merge 
+df = pd.merge(trans, cust, left_on='Sender Account ID', right_on='Customer ID', how='left')
+
+# Xóa cột thừa và Lưu file
+df = df.drop(columns=['Customer ID']) # Delete duplicate ID
+df = df.fillna(0) # write 0 in blank spaces
+
+save_path = f'dataset/merged/data.json'
+df.to_json (save_path, orient='records', indent=4)
+print("Done")
+S
